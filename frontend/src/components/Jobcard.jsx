c7cf49e6f01a35e42f303c9e3171f8e65449c20b
Prashantpatel698 <prashantkirar698@gmail.com>
2024-02-01 20:07:02 +0530

    job card name

--- frontend/src/components/Jobcard.jsx
+++ frontend/src/components/Jobcard.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 
 const Jobcard = () => {
   return (
-    <div >
+    <div > 
         <div className='h-[44vh] w-[20vw] bg-red-200 rounded-xl mb-16 ml-20'>
             <h1>name:tarun labda</h1>
+            <h1>jdhjdhjd</h1>
         </div>
     </div>
   )
