f8dc7bb66a09a6b851a3db4357a819f5aff53d17
Prashantpatel698 <prashantkirar698@gmail.com>
2024-02-01 19:46:59 +0530

    home and jobcard

---
+++ frontend/src/components/Jobcard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Jobcard = () => {
+  return (
+    <div >
+        <div className='h-[44vh] w-[20vw] bg-red-200 rounded-xl mb-16 ml-20'>
+            <h1>name:tarun labda</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Jobcard
